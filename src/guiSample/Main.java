216891de670aa05b3f45f2f2708fec263a52b4f2
Paaff <pafischer1992@gmail.com>
2016-02-23 21:57:08 +0100

    Har added in sample gui.
    Used Scenebuilder to create a sample gui.

---
+++ src/guiSample/Main.java
@@ -0,0 +1,28 @@
+package guiSample;
+
+/**
+ * Created by Paf on 23-02-2016.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        primaryStage.setTitle("Sudoku");
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.show();
+    }
+
+
+}
