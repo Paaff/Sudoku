32723bc1f6429889cba28da420b6d423c1013873
Paaff <pafischer1992@gmail.com>
2016-02-24 13:33:41 +0100

    Using SceneBuilder from now on. Adding a controller for the controls of the GUI.

---
+++ src/Controller.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Paf on 24-02-2016.
+ */
+public class Controller {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,26 @@
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+/**
+ * Created by Paf on 23-02-2016.
+ */
 
-// This is only a test - Mathias - 1 - 2 - 3 - Check!!
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Buttons testButton, testButton2, testButton3, testButton4, testButton5, testButton6;
-
     public static void main(String[] args) {
-
-        // Call the launch method in Application.
         launch(args);
-
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        // Set the stage.
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
         primaryStage.setTitle("Sudoku");
-
-
-        // Creating menu buttons
-        testButton = new Buttons("Start Game", true);
-        testButton.setOnAction(e -> System.out.println("Timer started."));
-
-        testButton2 = new Buttons("Set Difficulty", false);
-        testButton2.setOnAction(e -> System.out.println("Setting Difficulty"));
-
-        testButton3 = new Buttons("Options", false);
-        testButton3.setOnAction(e -> System.out.println("Entering Options."));
-
-        testButton4 = new Buttons("Generate Sudoku", false);
-        testButton4.setOnAction(e -> System.out.println("Generating Sudoku."));
-
-        testButton5 = new Buttons("Sudoku Solver", false);
-        testButton5.setOnAction(e -> System.out.println("Solving current Sudoku puzzle."));
-
-        testButton6 = new Buttons("Statistics", false);
-        testButton6.setOnAction(e -> System.out.println("Solver statitics."));
-
-
-
-        // Set the vertical layout
-        VBox menuLayout = new VBox();
-        menuLayout.setSpacing(30);
-        menuLayout.setPadding(new Insets(20, 20, 10, 20));
-        menuLayout.getChildren().addAll(testButton, testButton2, testButton3, testButton4, testButton5, testButton6);
-
-
-
-        // A scrollbar where the vertical layout resides.
-        ScrollPane scrollPane = new ScrollPane(menuLayout);
-
-
-        // Setting the main layout using embedded layout.
-        BorderPane layout = new BorderPane();
-        layout.setLeft(scrollPane);
-        // when I have something for the center  layout.setCenter(here);
-
-        // Set the scene.
-        Scene scene = new Scene(layout, 800, 600);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
     }
 
 
-
 }
-
