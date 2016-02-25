643dea4b3d65cc0bfc030aa60ed74230d96b6b33
paaff <pafischer1992@gmail.com>
2016-02-25 12:31:10 +0100

    Individual methods for each button clicked or one method that can differentiate between each button?

--- src/Controller.java
+++ src/Controller.java
@@ -3,9 +3,39 @@
  */
 public class Controller {
 
-        // Methods for button presses
+    // Methods for button presses
 
-    public void buttonClicked() {
-        System.out.print("Clicked start.");
+    public void startButtonClicked() {
+        System.out.println("Clicked start.");
     }
+    public void generateButtonClicked() {
+        System.out.println("Clicked Generate Puzzle.");
+    }
+    public void solveButtonClicked() {
+        System.out.println("Clicked Solve Puzzle.");
+    }
+    public void analyzeButtonClicked() {
+        System.out.println("Clicked Analyze Solver.");
+    }
+    public void optionsButtonClicked() {
+        System.out.println("Clicked Options.");
+    }
+    public void exitButtonClicked() {
+        System.out.println("Clicked Exit.");
+    }
+    public void hintButtonClicked() {
+        System.out.println("Clicked Hint.");
+    }
+    public void goBackOneStepButtonClicked() {
+        System.out.println("Clicked Go Back One Step.");
+    }
+    public void startTimerButtonClicked() {
+        System.out.println("Clicked Start Timer.");
+    }
+    public void pauseTimerButtonClicked() {
+        System.out.println("Clicked Pause Timer.");
+    }
+
+
 }
+
